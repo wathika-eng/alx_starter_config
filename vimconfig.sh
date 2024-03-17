fd2942883b39c3c35d337f4ade53497270006738
wathika <71040609+wathika-eng@users.noreply.github.com>
2024-03-17 19:18:21 +0300

    curl -sSL

--- vimconfig.sh
+++ vimconfig.sh
@@ -1,19 +1,28 @@
-#!/bin/env bash
+#!/bin/bash
+
+# Check if sudo is available
+if ! [ -x "$(command -v sudo)" ]; then
+  echo "Error: sudo is not installed. Please install sudo or run this script as root."
+  exit 1
+fi
 
 # Define the lines to be added to vimrc
-lines_to_add="set tabstop=8 shiftwidth=8
+read -r -d '' lines_to_add <<EOF
+set tabstop=8 shiftwidth=8
 set autoindent
 set smartindent
 set cindent
 syntax enable
-set number"
+set number
+EOF
 
 # Check if the file exists before attempting to modify it
 if [ -f /etc/vim/vimrc ]; then
     # Append lines to the vimrc file with sudo
-    echo -e "$lines_to_add" | sudo tee -a /etc/vim/vimrc > /dev/null
+    echo "$lines_to_add" | sudo tee -a /etc/vim/vimrc > /dev/null
     echo "Lines added to /etc/vim/vimrc successfully."
 else
     echo "Error: Vim configuration file (/etc/vim/vimrc) not found."
     exit 1
 fi
+
